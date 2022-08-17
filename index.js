60135698c3d38e1ba4f10bbfe668800b05a9ac6a
AndrewMartial <andrewomosh44@gmail.com>
2022-08-17 14:44:42 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+// divide 
+let divide = () => {
+    return 2000 / 100;
+}
+
+
+// square 
+let square = (x) => {
+    return x * x;
+}
+
+
+// add
+let add = (a, b) => {
+    return a + b
+}
+
+
+
+
+
+
+
+
+
+
